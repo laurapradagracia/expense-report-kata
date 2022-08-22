9a5780b3397d5518db476b852ff506ef5f5576b0
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-08-22 12:34:05 +0200

    test refactor for readability

--- test/ExpenseReportShould.cs
+++ test/ExpenseReportShould.cs
@@ -2,16 +2,18 @@ namespace ExpenseReports.Test;
 
 public class ExpenseReportShould
 {
+    private readonly ExpenseReport _expenseReport = new ExpenseReport();
+    private readonly StringWriter _stringWriter = new StringWriter();
+    private readonly DateTime _currentDate = new DateTime(2022, 08, 01);
+    public ExpenseReportShould()
+    {
+        Console.SetOut(_stringWriter);
+    }
     [Fact]
     public void Print_Empty_List()
     {
-        var stringWriter = new StringWriter();
-        Console.SetOut(stringWriter);
-
-        var date = new DateTime(2022, 08, 01);
-        var expectedDate = date.ToString();
-        var report = new ExpenseReport();
-        report.PrintReport(new List<Expense>(), date);
-        Assert.Equal($"Expenses {expectedDate}\r\nMeal expenses: 0\r\nTotal expenses: 0\r\n", stringWriter.ToString());
+        var expectedDate = _currentDate.ToString();
+        _expenseReport.PrintReport(new List<Expense>(), _currentDate);
+        Assert.Equal($"Expenses {expectedDate}\r\nMeal expenses: 0\r\nTotal expenses: 0\r\n", _stringWriter.ToString());
     }
 }
